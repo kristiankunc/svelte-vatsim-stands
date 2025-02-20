677e4f664c4bccbec67922b0cb60b794b61cf527
Kristián Kunc <kristian.kunc@gmail.com>
2025-02-20 18:15:22 +0100

    add more exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 import Map from "./map.svelte";
+import { tileUrls } from "./tiles.js";
+import { type StandData } from "./stands.js";
 
-export { Map };
+export { Map, tileUrls };
+export type { StandData };
